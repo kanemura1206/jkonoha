a516e63f80591e76377fd22cfb6c87380aebe05c
kanemura1206 <kanemura1206@gmail.com>
2012-05-22 15:56:46 +0900

    test

--- src/commons/konoha2/Kwb.java
+++ src/commons/konoha2/Kwb.java
@@ -6,9 +6,9 @@ public class Kwb {
 	KArray m;
 	int pos;
 	
-	public Kwb(KArray init) {
-		this.m = init;
-		this.pos = init.byteSize;
+	public Kwb(KArray m) {
+		this.m = m;
+		this.pos = m.byteSize;
 	}
 
 	public void write(CTX ctx, String data, int byteLen) {
@@ -16,12 +16,12 @@ public class Kwb {
 			/*karray_expand(_ctx, m, m->bytesize + byteLen);*/
 		}
 		/*memcpy(m.bytebuf + m.byteSize, data, byteLen);*/
-		m.bytebuf += m.byteSize + data.substring(0,byteLen + 1);	
+		m.bytebuf += m.byteSize + data.substring(0,byteLen + 1); // TODO
 		m.byteSize += byteLen;
 	}
 
-	public void putc(CTX ctx,int... ap) {
-		String buf = "";
+	public void Kwb_putc(CTX ctx,int... ap) {
+		String buf;
 		int len = 0;
 		for(int ch : ap) {
 			buf += ch;
@@ -30,33 +30,41 @@ public class Kwb {
 		write(ctx, buf, len);
 	}
 
-	public void vprintf(CTX ctx, Object... ap) {//String... ap
-		Object ap2 = ap;
-		int s = m.byteSize;
-		int n = vsnprintf( m.bytebuf + s, m.byteMax - s, ap);//TODO
-		if(n >= (m.byteMax - s) ) {
-			karray_expand(ctx, m, n + 1);//TODO
-			n = vsnprintf(m.bytebuf + s, m.byteMax - s, ap);
+	static void Kwb_vprintf(CTX, kwb_t *wb, const char *fmt, va_list ap) {
+		va_list ap2;
+		va_copy(ap2, ap);
+		karray_t *m = wb->m;
+		size_t s = m->bytesize;
+		size_t n = vsnprintf( m->bytebuf + s, m->bytemax - s, fmt, ap);
+		if(n >= (m->bytemax - s)) {
+			karray_expand(_ctx, m, n + 1);
+			n = vsnprintf(m->bytebuf + s, m->bytemax - s, fmt, ap2);
 		}
-		m.byteSize += n;
+		va_end(ap2);
+		m->bytesize += n;
 	}
 
-	public void printf (CTX ctx, Object... ap) {//TODO NO IDEA
-		vprintf(ctx, ap);
+	static void Kwb_printf(CTX, kwb_t *wb, const char *fmt, ...) {
+		va_list ap;
+		va_start(ap, fmt);
+		Kwb_vprintf(_ctx, wb, fmt, ap);
+		va_end(ap);
 	}
 
-	public String top(CTX ctx, boolean ensureZero) {
+	static const char* Kwb_top(CTX, kwb_t *wb, int ensureZero) {
+		karray_t *m = wb->m;
 		if(ensureZero) {
-			if(  !(m.byteSize + 1 < m.byteMax)  ) {
-				karray_expand(ctx, m, m.byteSize + 1);//TODO
+			if(!(m->bytesize + 1 < m->bytemax)) {
+				karray_expand(_ctx, m, m->bytesize + 1);
 			}
-			m.bytebuf += 0;//TODO NO IDEA
+			m->bytebuf[m->bytesize] = 0;
 		}
-		return (m.bytebuf + pos);
+		return (const char*)m->bytebuf + wb->pos;
 	}
 
-	public void free() {
-		bzero(m.bytebuf + pos, m.byteSize - pos);//TODO
-		m.byteSize = pos;
+	static void Kwb_free(kwb_t *wb) {
+		karray_t *m = wb->m;
+		bzero(m->bytebuf + wb->pos, m->bytesize - wb->pos);
+		m->bytesize = wb->pos;
 	}
 }
